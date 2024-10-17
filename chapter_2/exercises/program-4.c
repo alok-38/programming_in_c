5749b926a79af0eb6cd5936a93236f5d0510e99a
Alokananda Y <plantsfortrees@gmail.com>
2024-10-17 12:43:57 +0530

    Variables, Data Types, and Arithmetic Expressions

---
+++ chapter_2/exercises/program-4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    int value1 = 15;
+    int value2 = 87;
+    int difference = value2 - value1;
+    printf("%d subtracted from %d gives %d\n", value2, value1, difference);
+    return 0;
+}
+
